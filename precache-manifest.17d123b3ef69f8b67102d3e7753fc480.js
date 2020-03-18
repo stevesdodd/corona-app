74b4dfd46d8169323ab44536d951cd7b231d3363
stephen-dodd <stephen.dodd@rea-group.com>
2020-03-18 23:50:35 +1100

    Updates

---
+++ precache-manifest.17d123b3ef69f8b67102d3e7753fc480.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219390b95d16680b7f89f376ec20501f",
+    "url": "/corona-app/index.html"
+  },
+  {
+    "revision": "f5e423d16410809ab123",
+    "url": "/corona-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c1fd065a3124c33fd6e2",
+    "url": "/corona-app/static/js/2.25506119.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/corona-app/static/js/2.25506119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5e423d16410809ab123",
+    "url": "/corona-app/static/js/main.964c0bff.chunk.js"
+  },
+  {
+    "revision": "248208911c4b8d64afdc",
+    "url": "/corona-app/static/js/runtime-main.e426d2a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/corona-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
